8ceaf3603a0fe564a62671ff506f375795b18c2b
Jelena Roglic <jroglic4@gmail.com>
2024-12-08 18:52:03 +0100

    Azuriran server.js za povezivanje sa Railway bazom

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 3000; 
 
-const conn = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+const conn = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 const cors = require('cors');
 app.use(cors({
