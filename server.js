7ab144c751c50e69f7119cd374480037a8bc2d61
Jelena Roglic <jroglic4@gmail.com>
2024-12-08 17:58:46 +0100

    Added DB_PORT to MySQL connection and updated server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
